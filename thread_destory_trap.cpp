07c150e12f6f22ad283136cf8231d95b2ce4573d
papaivan <ningyigu@gmail.com>
2021-01-15 00:41:36 +0800

    Update thread_destory_trap.cpp

--- thread_destory_trap.cpp
+++ thread_destory_trap.cpp
@@ -1,1 +1,81 @@
+#include <thread>
+#include <string>
+#include <mutex>
 
+using namespace std;
+
+mutex g_mu;
+bool g_flag;
+
+class A {
+public:
+    A()
+    {
+        cout<<"A"<<endl;
+    }
+  
+    virtual ~A()
+    {
+        // 当析构函数被调用时，需要销毁什么？
+        // 释放内存，释放锁，文件描述符
+        
+    }
+  
+    OnHandleAction()
+    {   
+        // error, 非静态函数不能用来启动线程
+        thread threadFunc2(ThreadFunc);
+        threadFunc2.start();
+    }
+
+private:
+    void ThreadFunc2()
+    {
+        cout<<"ThreadFunc"<<endl;
+        g_mu.lock(); // 假如执行到这一行时线程crash，g_mu如何释放？
+        g_flag = false;
+        g_mu.unlock();
+    }
+    
+    void Func3()
+    {
+        cout<<"Func3"<<endl;
+    }
+    
+    void loop()
+    {
+        while (condition) {
+            cout<<"I am Func3"<<endl;
+            sleep(10);
+        }
+    }
+};
+
+
+void ThreadFunc()
+{
+    cout<<"ThreadFunc"<<endl;
+}
+
+void ThreadHandleA()
+{
+   A *a = new A();
+   a->Func3();
+   loop();
+   delete a;
+   a = nullptr;
+}
+
+int main()
+{
+   // it's ok
+   thread threadFunc(ThreadFunc);
+   threadFunc.start();
+   // 注意这里不detach走不到下面
+   threadFunc.detach();
+   
+   // it's ok
+   thread threadHandleA(ThreadHandleA);
+   threadHandleA.start();
+   threadHandleA.detach();
+}
